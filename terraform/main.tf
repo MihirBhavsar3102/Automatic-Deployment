5b92972a053f2c3dacd2714b61ecc6a7b650ab65
MihirBhavsar3102 <mihirbhavsar1995@gmail.com>
2025-02-13 11:06:43 +0530

    Add Terraform configuration for AWS infrastructure setup

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA5G2VG2TXYBMTCZ5A"
+  secret_key = "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"
+}
+
+# Defined the Security Group
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Defined the EC2 instance
+resource "aws_instance" "nginx_server" {
+  ami           = "ami-0c50b6f7dc3701ddd" 
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+  
+  # Used `vpc_security_group_ids` instead of `security_groups`
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "nginx-server"
+  }
+}
