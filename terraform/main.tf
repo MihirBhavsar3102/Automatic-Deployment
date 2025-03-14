3894f20679e7bcd8d0e28a22403831cbe42742ac
MihirBhavsar3102 <mihirbhavsar1995@gmail.com>
2025-03-14 14:36:08 +0530

    Update deployment workflow and Terraform configuration for SSH key handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_instance" "nginx_server" {
     command     = <<EOT
     echo "[web]" > ../ansible-setup/inventory
     echo "${aws_instance.nginx_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/mihir/.ssh/mihir.pem" >> ../ansible-setup/inventory
+    chmod 600 /home/mihir/.ssh/mihir.pem
 EOT
   }
 }
